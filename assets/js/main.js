dc544b99fdb90ff3ee347acad2093170e8a7419a
Rob Kososki <robkososki@me.com>
2021-03-22 17:01:18 -0400

    File Structure

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// function selectButtons(btn){
+// let x = document.querySelector(btn);
+// let y = x.className;
+// console.log(y);
+// };
+
+
+// function findTest(test){
+//     let x = document.querySelector('test').className;
+
+//     console.log(x);
+// }
+
+// const paragraph = document.querySelector("#testing");
+// console.log(paragraph);
+
+
+const test = document.querySelector ("#testing");
+console.log(test);
