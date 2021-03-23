fbe2820d3b022f05a6fa515158ca1656c7aae089
Rob Kososki <robkososki@me.com>
2021-03-23 06:18:07 -0400

    Hiding the buttons on click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,32 @@
-// function selectButtons(btn){
-// let x = document.querySelector(btn);
-// let y = x.className;
-// console.log(y);
+
+// How to select the inner HTML of an Element, in this case, an ID
+// const test = document.querySelector ("#btn-five");
+// console.log(test.innerHTML);
+
+// document.querySelector('#btn-five').onclick = function (){
+    
 // };
 
+// document.querySelector('#btn-five').style.backgroundColor === 'purple';
 
-// function findTest(test){
-//     let x = document.querySelector('test').className;
-
-//     console.log(x);
+// function removeButton (btnId){
+//     document.querySelector(btnId).onclick = function(){
+//         console.log(btnId.innerHTML);
+//     };
 // }
 
-// const paragraph = document.querySelector("#testing");
-// console.log(paragraph);
+// removeButton('#btn-five');
+
+// Selects the button and console logs its inner HTML when clicked
+// document.querySelector('#btn-five').onclick = function(){
+//     console.log(document.querySelector('#btn-five').innerHTML)
+// }
+
+// Selects the button and removes all buttons when clicked.
+document.querySelector('#btn-five').onclick = function(){
+    document.querySelector('#btn-five').remove();
+    document.querySelector('#btn-fifteen').remove();
+    document.querySelector('#btn-thirty').remove();
+}
 
 
-const test = document.querySelector ("#testing");
-console.log(test);
