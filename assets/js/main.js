7f46cb3f2fca0b11c772e688c49f7883e2d34072
Rob Kososki <robkososki@me.com>
2021-03-26 04:48:07 -0400

    Return button
    Got the buttons to appear again after return button is clicked my using visibility = "visible/hidden" instead of .remove()
    
    Now a new return button is added every time you select another time, or the same time again.
    
    Also, videos do not stop unless you hit pause, so the video just runs in the background.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,9 @@
-
 // How to select the inner HTML of an Element, in this case, an ID
 // const test = document.querySelector ("#btn-five");
 // console.log(test.innerHTML);
 
 // document.querySelector('#btn-five').onclick = function (){
-    
+
 // };
 
 // document.querySelector('#btn-five').style.backgroundColor === 'purple';
@@ -55,6 +54,9 @@
 // returnButton.onclick = function(){
 //     document.querySelector('body').innerHTML = body;
 // }
+
+
+
 const container = document.querySelector('#container');
 
 const body = document.querySelector('body');
@@ -69,65 +71,101 @@ const returnButtonContent = `
 `
 //Saves the HTML for the embeded video to a constant
 const fiveMinVideo = `
-<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/3RxXiFgkxGc" 
+<iframe width="560" height="315" id="youtube-video" src="https://www.youtube-nocookie.com/embed/3RxXiFgkxGc" 
 title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
 clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+const fifteenMinVideo = `
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/itZMM5gCboo?start=3" 
+title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+`
+constThirtyMinVideo    = `
+`
 //Saves the button Id's to variables
 let btnFive = document.querySelector('#btn-five');
 let btnFifteen = document.querySelector('#btn-fifteen');
 let btnThirty = document.querySelector('#btn-thirty');
 
-btnFive.onclick = function(){
-//Uses the variables to remove the buttons when the user clicks on them.
-    btnFive.remove();
-    btnFifteen.remove();
-    btnThirty.remove();
-//Creates a div and saves it to a constant
+btnFive.onclick = function () {
+    //Makes the video and return button visible again for the 2nd clickthrough
+    video.style.visibility = 'visible';
+    returnButton.style.visibility = 'visible';
+    //Uses the variables to remove the buttons when the user clicks on them.
+    btnFive.style.visibility = 'hidden';
+    btnFifteen.style.visibility = 'hidden';
+    btnThirty.style.visibility = 'hidden';
+    //Creates a div and saves it to a constant
     const videoElement = document.createElement('div');
-//Set's the ID of that div to five-min-video
+    //Set's the ID of that div to five-min-video
     videoElement.setAttribute('class', 'video-style')
     videoElement.setAttribute('id', 'video')
-//Injects the embeded video HTML into the new div
+    //Injects the embeded video HTML into the new div
     videoElement.innerHTML = fiveMinVideo;
-//Places the new div under the main-button id 
+    //Places the new div under the main-button id 
     videoDiv.append(videoElement);
-//When button is clicked, changes the background color to black
+    //When button is clicked, changes the background color to black
     body.style.backgroundColor = "black";
-//Creates a new div which will house the return button
+    //Creates a new div which will house the return button
     const returnButtonElement = document.createElement('div');
-//Creating a class on that div called return-button-style
-    returnButtonElement.setAttribute('class','return-button-style');
-//Creating an ID on that div
-    returnButtonElement.setAttribute('id','return-button');
-//Sets the inner HTML of that div to the content in the returnButtonContent variable
+    //Creating a class on that div called return-button-style
+    returnButtonElement.setAttribute('class', 'return-button-style');
+    //Creating an ID on that div
+    returnButtonElement.setAttribute('id', 'return-button');
+    //Sets the inner HTML of that div to the content in the returnButtonContent variable
     returnButtonElement.innerHTML = returnButtonContent;
-//Appends the new element with content to the return button div in the DOM
+    //Appends the new element with content to the return button div in the DOM
     returnButtonDiv.append(returnButtonElement);
 }
 returnButton = document.querySelector('#return-button');
 const video = document.querySelector('#video')
-returnButton.onclick = function(){
-    body.style.backgroundColor = "white";
-    video.remove();
-    returnButton.remove();
-    body.append(container);
+returnButton.onclick = function () {
+    fiveMinVideo.pause();
+    body.style.backgroundColor = 'rebeccapurple';
+    video.style.visibility = 'hidden';
+    returnButton.style.visibility = 'hidden';
+    btnFive.style.visibility = 'visible';
+    btnFifteen.style.visibility = 'visible';
+    btnThirty.style.visibility = 'visible';
 }
 
-btnFifteen.onclick = function(){
-    btnFive.remove();
-    btnFifteen.remove();
-    btnThirty.remove();
+btnFifteen.onclick = function () {
+    video.style.visibility = 'visible';
+    returnButton.style.visibility = 'visible';
+    btnFive.style.visibility = 'hidden';
+    btnFifteen.style.visibility = 'hidden';
+    btnThirty.style.visibility = 'hidden';
+    const videoElement = document.createElement('div');
+    videoElement.setAttribute('class', 'video-style')
+    videoElement.setAttribute('id', 'video')
+    videoElement.innerHTML = fifteenMinVideo;
+    videoDiv.append(videoElement);
+    body.style.backgroundColor = "#ebc99e";
+    const returnButtonElement = document.createElement('div');
+    returnButtonElement.setAttribute('class', 'return-button-style');
+    returnButtonElement.setAttribute('id', 'return-button');
+    returnButtonElement.innerHTML = returnButtonContent;
+    returnButtonDiv.append(returnButtonElement);
 }
-btnThirty.onclick = function (){
-    btnFive.remove();
-    btnFifteen.remove();
-    btnThirty.remove();
+btnThirty.onclick = function () {
+    video.style.visibility = 'visible';
+    returnButton.style.visibility = 'visible';
+    btnFive.style.visibility = 'hidden';
+    btnFifteen.style.visibility = 'hidden';
+    btnThirty.style.visibility = 'hidden';
+    const videoElement = document.createElement('div');
+    videoElement.setAttribute('class', 'video-style')
+    videoElement.setAttribute('id', 'video')
+    videoElement.innerHTML = fiveMinVideo;
+    videoDiv.append(videoElement);
+    body.style.backgroundColor = "black";
+    const returnButtonElement = document.createElement('div');
+    returnButtonElement.setAttribute('class', 'return-button-style');
+    returnButtonElement.setAttribute('id', 'return-button');
+    returnButtonElement.innerHTML = returnButtonContent;
+    returnButtonDiv.append(returnButtonElement);
 }
 
 
 
 
-
-
-//Slowy make the background color transition to black
 
